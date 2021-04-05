c4af9fe9f7a95de2d2c93de51a6dd4999db60519
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-04-05 17:16:34 -0500

    final

--- forms/CustomerStates/CustomerStates.js
+++ forms/CustomerStates/CustomerStates.js
@@ -6,7 +6,7 @@ let results = []
 
 CustomerStates.onshow = function() {
    drpStates.clear() 
-  query = "SELECT DISTINCT `state` FROM customer GROUP BY `state`"
+  query = "SELECT DISTINCT `state` FROM customer"
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
   
   if (req.status == 200) { //transit trip worked. 
@@ -45,8 +45,8 @@ drpStates.onclick=function(s){
            txtaCustomerStates.value = `The customer(s) in ${state} are: \n` + message
         } // end else
 
-    } else   // the transit didn't work - bad wifi? server turned off?
-        lblCustomers.value = "Error code: " + req.status
+      } else   // the transit didn't work - bad wifi? server turned off?
+          lblCustomers.value = "Error code: " + req.status
 
  }
 } 
